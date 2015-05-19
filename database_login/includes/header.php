591ab8f1eedb6949bee4ac3095645790bd798135
kanzagilani <kanzagilani@gmail.com>
2015-05-19 13:40:40 +0500

    first commit

---
+++ database_login/includes/header.php
@@ -0,0 +1,33 @@
+<div class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-sm-3 col-md-3 col-xs-12 hidden-xs">
+        <div class="logo"> <a href="#"><img src="images/logo.png" class="img-responsive" alt=""/></a> </div>
+      </div>
+      <div class="col-lg-9 col-sm-9 col-md-9 col-xs-12 no-padding">
+        <nav class="navbar navbar-default bg" role="navigation"> 
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          </div>
+          
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="inner-page.php">Medicine Assistant</a></li>
+              <li><a href="#">Exercise Assistant</a></li>
+              <li><a href="#">Food Assistant</a></li>
+              <li><a href="#">Healthcare Assistant</a></li>
+               <li><a href="#">BpSimulation Assistant</a></li>
+               <li><a href="signout.php">Sign Out</a></li>
+              
+            </ul>
+          </div>
+          <!-- /.navbar-collapse --> 
+          
+        </nav>
+      </div>
+      
+    </div>
+  </div>
+</div>
